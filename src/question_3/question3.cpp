6383a557cb4bee91810c387d35c77a214ef472d8
rob <rob@localhost.localdomain>
2025-03-12 19:01:30 -0600

    Question 3 edit

--- src/question_3/question3.cpp
+++ src/question_3/question3.cpp
@@ -15,9 +15,7 @@ double get_dna_p_distance(const string &parameter1, const string &parameter2){
     // if (parameter1.length() = parameter2.length() && parameter1.length() != 0){}
     double numerator = 0;
     for(int letter = parameter1.length() - 1; letter >= 0; letter--){
-        // std::cout << letter << "meow " << parameter1[letter] << parameter2[letter] << "\n";
         if (parameter1[letter] != parameter2[letter]){
-            // std::cout << "bam\n";
             numerator++;
         }
     }
